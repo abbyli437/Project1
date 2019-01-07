dc66322f965f19aa9ce535c03889bf4f909e8c01
Abby Li <s-liab@51S50023399.it.bsd405.org>
2019-01-07 08:09:09 -0800

    updating

--- Project1/Board.java
+++ Project1/Board.java
@@ -9,6 +9,22 @@ public class Board
 	private int remaining; tiles remaining to be clicked 
 	private int mines;
 	
+//	//adding tile class in here to see if it does anything, might uncomment as a last resort 
+//	private class Tile 
+//	{
+//		int adj;
+//		boolean isMine;
+//		boolean flagged;
+//		boolean clicked;
+//		
+//		public Tile()
+//		{
+//			isMine = false;
+//			flagged = false;
+//			clicked = false;
+//		}
+//	}
+	
 	public Board(int rows, int cols, int numMines)
 	{
 		board = new Tile[rows][cols];
@@ -56,7 +72,48 @@ public class Board
 		while (count > 0)
 		{
 			int index = (int) (Math.random() * list.size());
-			board[index / cols][index % cols].
+			int location = list.remove(index);
+			board[location / cols][location % cols].isMine = true;
+			count--;
+		}
+		//sets up all the locations where there's mines 
+		
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				
+			}
+		}
+	}
+	
+	private void markMines(int i, int j)
+	{
+		if (board[i][j].isMine == false) //what's with isMine not being blue?
+		{
+			if (i == 0)
+			{
+				if (j == 0) //top left corner 
+				{
+					if (board[0][1].isMine)
+						board[0][1].adj++;
+					if (board[1][1].isMine)
+						board[1][1].adj++;
+					if (board[1][0].isMine)
+						board[1][0]++;
+				}
+				else if (j == rows - 1)
+				{
+					
+				}
+				else
+				{
+					for (int i = 0; i < 2; i++)
+					{
+						
+					}
+				}
+			}
 		}
 	}
 }
